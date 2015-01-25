26f5384eb7d1cecb8231b5dd4639b8521c1a50fd
Kimmo Koskinen <kimmo.koskinen@solita.fi>
2015-01-25 13:04:27 +0200

    Strip things apart for just evaluating forms

--- project.clj
+++ project.clj
@@ -4,7 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.datomic/datomic-free "0.9.5130"]]
-  :main ^:skip-aot puihin.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+                 [com.datomic/datomic-free "0.9.5130"]])
--- src/puihin/core.clj
+++ src/puihin/core.clj
@@ -1,6 +1,5 @@
 (ns puihin.core
-  (:require [datomic.api :as d])
-  (:gen-class))
+  (:require [datomic.api :as d]))
 
 (def db-uri-base "datomic:mem://")
 
@@ -37,17 +36,43 @@
       [{:node/text "leaf1"}
        {:node/text "leaf2"}]}]}])
 
-(defn -main [& args]
-  (let [conn (scratch-conn)
-        temp-root-id (d/tempid :db.part/user)]
-    (d/transact conn schema)
-    (let [tempids (:tempids @(d/transact conn (document-like-tree-data temp-root-id)))
-          root-entity-id (d/resolve-tempid (d/db conn) tempids temp-root-id)]
-      (println "The whole tree as a map with query:" '[*])
-      (clojure.pprint/pprint (d/pull (d/db conn) '[*] root-entity-id))
-      (println "Only leafs with query:" '[{:node/children
-                                           [{:node/children
-                                             [:node/text]}]}])
-      (clojure.pprint/pprint (d/pull (d/db conn) '[{:node/children
-                                                    [{:node/children
-                                                      [:node/text]}]}] root-entity-id)))))
+(def conn (scratch-conn))
+
+(def temp-root-id (d/tempid :db.part/user))
+
+; Schema installed with a transaction
+(d/transact conn schema)
+
+; Now let's put data in
+(def tempids (:tempids @(d/transact conn (document-like-tree-data temp-root-id))))
+
+; Get entity id for the root
+(def root-entity-id (d/resolve-tempid (d/db conn) tempids temp-root-id))
+
+(def whole-tree-query '[*])
+
+(println "The whole tree as a map with query:" whole-tree-query)
+(clojure.pprint/pprint (d/pull (d/db conn) whole-tree-query root-entity-id))
+
+(def leafs-query '[{:node/children
+                    [{:node/children
+                      [:node/text]}]}])
+(println "Only leafs with query:" leafs-query)
+(clojure.pprint/pprint (d/pull (d/db conn) leafs-query root-entity-id))
+
+;; http://www.jayway.com/2012/06/27/finding-out-who-changed-what-with-datomic/
+
+;; need schema first
+(d/transact conn [[:db/add root-entity-id :node/audit-user "John Doe"]])
+
+(def audit-user-schema [{:db/id #db/id[:db.part/db]
+                         :db/ident :node/audit-user
+                         :db/valueType :db.type/string
+                         :db/cardinality :db.cardinality/one
+                         :db/fulltext true
+                         :db/index true
+                         :db.install/_attribute :db.part/db}])
+(d/transact conn audit-user-schema)
+
+; Now it should work
+(d/transact conn [[:db/add root-entity-id :node/audit-user "John Doe"]])
